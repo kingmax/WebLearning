f1627c7040e86b7a62c5d98d827b02072dba0c5c
iJasonLee <kingmax_res@163.com;184327932@qq.com>
2017-09-25 00:23:03 +0800

    + PHP Valid Inputs

---
+++ wwwroot/welcom_get.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YaJun
+ * Date: 2017/9/24
+ * Time: 9:05
+ */
+echo "Your name: " . $_GET['name'], "<br>";
+echo "Your email:" . $_GET['email'], "<br>";
---
+++ wwwroot/welcom_post.php
@@ -0,0 +1,22 @@
+<html>
+
+<title>welcom</title>
+
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YaJun
+ * Date: 2017/9/24
+ * Time: 8:41
+ */
+
+echo "Welcom " . $_POST["name"], "<br>";
+echo "Your email address is: " . $_POST["email"], "<br>";
+
+?>
+
+</body>
+
+</html>
